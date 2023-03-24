bcd65ae4047deac35ad6ed665a639a9754f2a7b9
ralajan <raghava.alajangi@mpl.mpg.de>
2023-03-24 16:03:32 +0100

    feat: added hpc_pipeline_request issues data

--- hpc_pipeine_app/main_page.py
+++ hpc_pipeine_app/main_page.py
@@ -1,66 +1,79 @@
-from dash import html, dcc, callback, Input, Output, State
+import pathlib
+
 import dash_bootstrap_components as dbc
+from dash import html, dcc, callback, Input, Output, State
+
+from ..components import dropdown_menu_comp, paragraph_comp, group_accordion
+from ..gitlab_api import GitLabAPI
+
+token_path = pathlib.Path(__file__).parents[1] / "SECRETS.txt"
+
+with open(token_path) as f:
+    token = f.readlines()[0]
+
+gitlab = GitLabAPI(token=token)
+
+opened = gitlab.get_issues_meta(state="opened")
+closed = gitlab.get_issues_meta(state="closed")
+
+# print(closed)
+print(opened)
 
 
-# from .. import gitlab_api
-#
-# issues_opened = gitlab_api.get_issues(status="opened")
-# issues_closed = gitlab_api.get_issues(status="closed")
-# meta_data_open = gitlab_api.get_issues_metadata(issues_opened)
-# meta_data_close = gitlab_api.get_issues_metadata(issues_closed)
-
-
-def main_request_card():
+def main_tab_content():
+    dropdown_comp1 = dbc.DropdownMenuItem([
+        dbc.NavLink("Simple",
+                    href="/hpc_pipelines/simple",
+                    id="open_simple_request_page",
+                    active=True)
+    ])
+    dropdown_comp2 = dbc.DropdownMenuItem([
+        dbc.NavLink("Advanced",
+                    href="/hpc_pipelines/advanced",
+                    id="open_advance_request_page",
+                    active=True),
+    ])
     return dbc.Card([
+        html.Br(), html.Br(),
+        paragraph_comp("⦿ This page is responsible for running RTDC "
+                       "dataset processing pipelines on MPCDF gpu "
+                       "clusters (HPC)",
+                       indent=40),
         html.Br(),
-        html.P("⦿ This page is responsible for running RTDC dataset "
-               "processing pipelines on MPCDF gpu clusters (HPC)"),
-        html.Br(),
-
-        dbc.DropdownMenu([
-            dbc.DropdownMenuItem([
-                dbc.NavLink("Simple",
-                            href="/hpc_pipelines/simple",
-                            id="open_simple_request_page",
-                            active=True),
-            ]),
-            dbc.DropdownMenuItem([
-                dbc.NavLink("Advanced",
-                            href="/hpc_pipelines/advanced",
-                            id="open_advance_request_page",
-                            active=True),
-            ]),
-        ],
-            label="New Request",
-        )
+        dropdown_menu_comp(name="New Request",
+                           components=[dropdown_comp1, dropdown_comp2],
+                           indent=40),
     ],
         className="mt-3",
         style={"height": "50rem", 'background-color': "#424447"}
     )
 
 
-def opened_request_card(open_issues):
+def opened_tab_content(open_issues):
     return dbc.Card([
-        dbc.CardBody(
-            html.Div(
-                open_issues,
-                id="open_issue_card",
-                # style={"max-height": "50rem", "overflow-y": "scroll"},
-            ),
-        )
+        html.Br(), html.Br(),
+        html.Div(
+            open_issues,
+            style={"max-height": "50rem", "overflow-y": "scroll",
+                   "overflowX": "hidden"},
+        ),
+        html.Br(), html.Br(),
     ],
         className="mt-3",
         style={"height": "50rem", 'background-color': "#424447"}
     )
 
 
-def closed_request_card():
-    return dbc.Card(
-        dbc.CardBody([
-            html.P("This is tab 2!", className="card-text"),
-            dbc.Button("Don't click here", color="danger"),
-        ]),
-        id="close_issue_card",
+def closed_tab_content(close_issues):
+    return dbc.Card([
+        html.Br(), html.Br(),
+        html.Div(
+            close_issues,
+            style={"max-height": "50rem", "overflow-y": "scroll",
+                   "overflowX": "hidden"},
+        ),
+        html.Br(), html.Br(),
+    ],
         className="mt-3",
         style={"height": "50rem", 'background-color': "#424447"}
     )
@@ -70,7 +83,7 @@ def request_tabs():
     return dbc.Card([
         dbc.CardHeader(
             dbc.Tabs([
-                dbc.Tab(label="Main tab",
+                dbc.Tab(label="Main",
                         tab_id="main_tab",
                         active_label_style={"color": "#017b70"}),
                 dbc.Tab(label="Open requests",
@@ -91,42 +104,73 @@ def request_tabs():
 def main_layout():
     return html.Div([
         request_tabs(),
-        dcc.Store(id='store_gitlab_issues', data={})
-        # data={"closed_metadata": meta_data_close,
-        #       "opened_metadata": meta_data_open}),
+        dcc.Store(id='store_gitlab_issues',
+                  data={"opened": opened,
+                        "closed": closed}),
     ])
+
+
+def get_issue_accord(data):
+    return group_accordion([
+        dbc.AccordionItem([
+            paragraph_comp(text="Issue created by: {}".format(c["author"])),
+            html.A(
+                "See the issue on GitLab",
+                href=c["web_url"],
+                target='_blank',
+                style={"text-decoration": "none"}
+            ),
+            # html.Br(), html.Br(),
+            # dbc.Card([
+            #     dbc.Card(co) for co in c["comments"]
+            # ]),
+        ],
+            title=c["name"],
+        ) for c in data
+    ],
+        middle=True, width=60
+    )
 
 
 @callback([Output("tab_content", "children"),
            Output("store_gitlab_issues", "data")],
           Input("tabs", "active_tab"),
           State("store_gitlab_issues", "data"))
-def switch_tab(click_tab, issue_data):
+def switch_tab(click_tab, stored_issue_meta):
     if click_tab == "main_tab":
-        return [main_request_card(), issue_data]
+        return [main_tab_content(), stored_issue_meta]
     elif click_tab == "opened_tab":
-        # opened_issues = gitlab_api.get_issues(status="opened")
-        # opened_issues_meta = gitlab_api.get_issues_metadata(opened_issues)
-        # stored_opened_meta = issue_data["opened_metadata"]
-        #
-        # if len(stored_opened_meta) == len(opened_issues_meta):
-        #     issue_data["opened_metadata"] = stored_opened_meta
-        # else:
-        #     issue_data["opened_metadata"] = opened_issues_meta
-        #
-        # toasts = dbc.Accordion([
-        #     dbc.AccordionItem([
-        #         html.P(c["author"], style={"color": "white"}),
-        #         html.P(c["web_url"], style={"color": "white"}),
-        #     ],
-        #         title=c["name"],
-        #     ) for c in issue_data["opened_metadata"]
-        # ])
-        return [opened_request_card([]), issue_data]
+        open_issues_meta = gitlab.get_issues_meta(state="opened")
+
+        if len(open_issues_meta) == 0:
+            open_issues = paragraph_comp(text="⦿ We do not have opened "
+                                              "requests!", indent=40)
+            return [opened_tab_content(open_issues), stored_issue_meta]
+        else:
+            stored_open_meta = stored_issue_meta["opened"]
+            if len(stored_open_meta) == len(open_issues_meta):
+                stored_issue_meta["opened"] = stored_open_meta
+            else:
+                stored_issue_meta["opened"] = open_issues_meta
+
+            open_issues = get_issue_accord(stored_issue_meta["opened"])
+
+            return [opened_tab_content(open_issues), stored_issue_meta]
 
     elif click_tab == "closed_tab":
-        # closed_issues = gitlab_api.get_issues(status="closed")
-        # closed_issues_meta = gitlab_api.get_issues_metadata(closed_issues)
-        # stored_opened_meta = issue_data["closed_metadata"]
-        return [closed_request_card(), issue_data]
+        close_issues_meta = gitlab.get_issues_meta(state="closed")
+
+        if len(close_issues_meta) == 0:
+            close_issues = paragraph_comp(text="⦿ We do not have closed "
+                                               "requests!", indent=40)
+            return [closed_tab_content(close_issues), stored_issue_meta]
+        else:
+            stored_open_meta = stored_issue_meta["closed"]
+            if len(stored_open_meta) == len(close_issues_meta):
+                stored_issue_meta["closed"] = stored_open_meta
+            else:
+                stored_issue_meta["closed"] = close_issues_meta
+
+            close_issues = get_issue_accord(stored_issue_meta["closed"])
+        return [closed_tab_content(close_issues), stored_issue_meta]
     return html.P("This shouldn't ever be displayed...")
