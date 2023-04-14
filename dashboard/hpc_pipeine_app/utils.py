ab952cde82d85ccec89cdef1dcf3f6f22d31bed0
ralajan <raghava.alajangi@mpl.mpg.de>
2023-04-14 11:18:30 +0200

    reset template before update

--- dashboard/hpc_pipeine_app/utils.py
+++ dashboard/hpc_pipeine_app/utils.py
@@ -4,9 +4,10 @@ simple_template, advanced_template = gitlab_api.get_templates()
 
 
 def update_simple_template(params, template=simple_template):
-    # global simple_template
+    # Uncheck all the boxes in the template before update
+    template = template.replace(f"[x]", f"[ ]")
     params_lower = [p.lower() for p in params]
-    for pl, p in zip(params_lower, params):
-        if pl in params_lower and pl in template.lower():
-            template = template.replace(f"[ ] {p}", f"[x] {p}")
+    for p_low, p_nrm in zip(params_lower, params):
+        if p_low in params_lower and p_low in template.lower():
+            template = template.replace(f"[ ] {p_nrm}", f"[x] {p_nrm}")
     return template
