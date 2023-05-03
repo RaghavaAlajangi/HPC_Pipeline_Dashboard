fcab06c5be5e58b784c7102b384a3d07eda6adf8
ralajan <raghava.alajangi@mpl.mpg.de>
2023-05-03 16:00:07 +0200

    enh: update template with rtdc paths

--- dashboard/hpc_pipeine_app/utils.py
+++ dashboard/hpc_pipeine_app/utils.py
@@ -1,13 +1,21 @@
-from ..gitlab_api import gitlab_api
-
-simple_template, advanced_template = gitlab_api.get_templates()
-
-
-def update_simple_template(params, template=simple_template):
+def update_simple_template(params, rtdc_paths, template):
     # Uncheck all the boxes in the template before update
     template = template.replace("[x]", "[ ]")
     params_lower = [p.lower() for p in params]
     for p_low, p_nrm in zip(params_lower, params):
-        if p_low in params_lower and p_low in template.lower():
+        if p_low in template.lower():
+            template = template.replace(f"[ ] {p_low}", f"[x] {p_low}")
+        elif p_nrm in template:
             template = template.replace(f"[ ] {p_nrm}", f"[x] {p_nrm}")
+
+    template = template.split("- **Data to Process**")[0]
+    template = template + "- **Data to Process**"
+
+    for path in rtdc_paths:
+        template = template + f"\n   - [x] {path}"
+
     return template
+
+
+def update_advanced_template(params, rtdc_paths, template):
+    pass
