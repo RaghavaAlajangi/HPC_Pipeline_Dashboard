b5ef3bac936fa5f191ea491b7259583b2069e15c
ralajan <raghava.alajangi@mpl.mpg.de>
2023-08-10 18:01:14 +0200

    enh: optimze code

--- dashboard/app.py
+++ dashboard/app.py
@@ -28,56 +28,66 @@ def wrong_page(pathname):
 
 
 def sidebar_menu():
-    return html.Div([
-        groupby_columns([
-            html.H3("HPC Pipelines", style={"font-weight": "bold"}),
-            line_breaks(times=1)
-        ]),
-        dbc.Alert([
-            # Warning icon
-            html.I(className="bi bi-exclamation-triangle-fill me-2"),
-            "Note:",
-            paragraph_comp(
-                text="Please be aware that running a pipeline is "
-                     "computationally expensive so please do not trigger or "
-                     "create unnecessary pipelines!",
-                comp_id="dummy2", indent=1
+    return html.Div(
+        [
+            groupby_columns(
+                [
+                    html.H3("HPC Pipelines", style={"font-weight": "bold"}),
+                    line_breaks(times=1)
+                ]
             ),
-        ],
-            style={"color": "black", "width": "fit-content"},
-            color="warning",
-        ),
-        groupby_columns([
-            line_breaks(times=1),
-            dbc.Nav([
-                dbc.NavLink("Home",
-                            href="/hpc-pipelines/",
-                            id="home_active"),
-                dbc.NavLink("Simple request",
-                            href="/hpc-pipelines/simple_request",
-                            id="simple_request_active"),
-                dbc.NavLink("Advanced request",
-                            href="/hpc-pipelines/advanced_request",
-                            id="advanced_request_active"),
-            ],
-                vertical=True,
-                pills=True,
+            dbc.Alert(
+                [
+                    # Warning icon
+                    html.I(className="bi bi-exclamation-triangle-fill me-2"),
+                    "Note:",
+                    paragraph_comp(
+                        text="Please be aware that running a pipeline is "
+                             "computationally expensive so please do not trigger or "
+                             "create unnecessary pipelines!",
+                        comp_id="dummy2", indent=1
+                    ),
+                ],
+                style={"color": "black", "width": "fit-content"},
+                color="warning",
             ),
-            line_breaks(times=1),
-        ]),
+            groupby_columns(
+                [
+                    line_breaks(times=1),
+                    dbc.Nav(
+                        [
+                            dbc.NavLink("Home",
+                                        href="/hpc-pipelines/",
+                                        id="home_active"),
+                            dbc.NavLink("Simple request",
+                                        href="/hpc-pipelines/simple_request",
+                                        id="simple_request_active"),
+                            dbc.NavLink("Advanced request",
+                                        href="/hpc-pipelines/advanced_request",
+                                        id="advanced_request_active"),
+                        ],
+                        vertical=True,
+                        pills=True,
+                    ),
+                    line_breaks(times=1),
+                ]
+            ),
 
-        html.Div([
-            line_breaks(times=5),
-            html.A([
-                html.Img(src=GITLAB_LOGO_URL, height=50),
-                " Source code"
-            ],
-                href=PROJECT_REPO_URL,
-                target="_blank",
-                style={"text-decoration": "none"}
+            html.Div(
+                [
+                    line_breaks(times=5),
+                    html.A(
+                        [
+                            html.Img(src=GITLAB_LOGO_URL, height=50),
+                            " Source code"
+                        ],
+                        href=PROJECT_REPO_URL,
+                        target="_blank",
+                        style={"text-decoration": "none"}
+                    )
+                ]
             )
-        ])
-    ],
+        ],
         id="sidebar"
     )
 
@@ -101,32 +111,35 @@ app = dash.Dash(
     # see: https://www.w3schools.com/css/css_rwd_viewport.asp for more
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}
-    ])
+    ]
+)
 
 server = app.server
 
 # Build the layout of the app
-app.layout = html.Div([
-    dcc.Location(id="url"),
-    dcc.Location(id="refresh_app", refresh=True),
-    sidebar_menu(),
-    main_content_block(),
-])
+app.layout = html.Div(
+    [
+        dcc.Location(id="url"),
+        dcc.Location(id="refresh_app", refresh=True),
+        sidebar_menu(),
+        main_content_block(),
+    ]
+)
 
 
 @app.callback(
-    [Output("page-content", "children"),
-     Output("home_active", "active"),
-     Output("simple_request_active", "active"),
-     Output("advanced_request_active", "active")],
+    Output("page-content", "children"),
+    Output("home_active", "active"),
+    Output("simple_request_active", "active"),
+    Output("advanced_request_active", "active"),
     Input("url", "pathname"),
 )
 def render_page_content(pathname):
     if pathname == "/hpc-pipelines/":
-        return [main_layout(), True, False, False]
+        return main_layout(), True, False, False
     elif pathname == "/hpc-pipelines/simple_request":
-        return [simple_request(), False, True, False]
+        return simple_request(), False, True, False
     elif pathname == "/hpc-pipelines/advanced_request":
-        return [advanced_request(), False, False, True]
+        return advanced_request(), False, False, True
     # Return a 404 message, if user tries to reach undefined page
     return wrong_page(pathname)
