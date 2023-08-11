6fc6a7d45c476aa79843fd1ee987d7df98d72ff0
ralajan <raghava.alajangi@mpl.mpg.de>
2023-08-11 18:15:05 +0200

    enh: filtering is improved. Update chunk index file continuously istead of creating at the end

--- resources/cache_handler.py
+++ resources/cache_handler.py
@@ -4,8 +4,10 @@ import pickle
 import os
 import time
 
-HSM_PATH = Path(__file__).parents[1] / "HSMFS"
+# HSM_PATH = Path(__file__).parents[1] / "HSMFS"
 RESOURCE_PATH = Path(__file__).parents[1] / "resources"
+
+HSM_PATH = "U:/"
 
 
 class HSMDataProcessor:
@@ -17,11 +19,11 @@ class HSMDataProcessor:
         self.chunk_idx_file_path = resource_path / "hsm_chunk_index.pkl"
         if not self.chunk_dir_path.exists():
             self.chunk_dir_path.mkdir(parents=True, exist_ok=True)
-        self.data = []
         self.chunk_data = []
         self.chunk_index = {}
 
     def clear_chunk_directory(self):
+        """Erase chunk data and index files from resource dir."""
         if self.chunk_idx_file_path.exists():
             self.chunk_idx_file_path.unlink()
         for item in self.chunk_dir_path.iterdir():
@@ -30,17 +32,22 @@ class HSMDataProcessor:
             elif item.is_dir():
                 item.rmdir()
 
-    def update_chunk_index(self, chunk_data, chunk_id):
+    def save_chunk_data(self, chunk_data, chunk_id):
+        """Save the chunk data as a pickle file in hsm_chunk_dir."""
+        chunk_path = self.chunk_dir_path / f"chunk_num_{chunk_id}.pkl"
+        with open(chunk_path, "wb") as f:
+            pickle.dump(chunk_data, f)
+
+    def update_and_save_chunk_index(self, chunk_data, chunk_id):
+        """Update the chunk index and save it."""
         for entry in chunk_data:
             for keyword in entry["filepath"]:
                 if keyword not in self.chunk_index:
                     self.chunk_index[keyword] = set()
                 self.chunk_index[keyword].add(chunk_id)
 
-    def save_chunk_to_pickle(self, chunk_data, chunk_id):
-        chunk_path = self.chunk_dir_path / f"chunk_num_{chunk_id}.pkl"
-        with open(chunk_path, "wb") as f:
-            pickle.dump(chunk_data, f)
+        with open(str(self.chunk_idx_file_path), "wb") as f:
+            pickle.dump(self.chunk_index, f)
 
     def process_hsmdrive(self):
         t1 = time.time()
@@ -64,23 +71,22 @@ class HSMDataProcessor:
                     "filepath": fpath.split("/"),
                     "dateModified": modified_time
                 }
-                self.data.append(entry)
-
                 chunk_data.append(entry)
 
                 # Pickle the chunk data when it reaches the chunk size
                 if len(chunk_data) >= self.chunk_size:
                     chunk_counter += 1
-                    self.save_chunk_to_pickle(chunk_data, chunk_counter)
-                    # Update chunk index for filtering
-                    self.update_chunk_index(chunk_data, chunk_counter)
+                    self.save_chunk_data(chunk_data, chunk_counter)
+                    # Update and save the final chunk index
+                    self.update_and_save_chunk_index(chunk_data, chunk_counter)
                     chunk_data = []
 
         # Pickle any remaining data if it's not a full chunk
         if chunk_data:
-            self.save_chunk_to_pickle(chunk_data, chunk_counter)
-            # Update chunk index for filtering
-            self.update_chunk_index(chunk_data, chunk_counter)
+            chunk_counter += 1
+            self.save_chunk_data(chunk_data, chunk_counter)
+            # Update and save the final chunk index
+            self.update_and_save_chunk_index(chunk_data, chunk_counter)
 
         # Save the chunk index file
         with open(str(self.chunk_idx_file_path), "wb") as f:
