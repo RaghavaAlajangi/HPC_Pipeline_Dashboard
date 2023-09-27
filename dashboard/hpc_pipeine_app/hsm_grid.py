41ffa3a58a97268b46586a36d9626a66405ab8db
ralajan <raghava.alajangi@mpl.mpg.de>
2023-09-27 11:53:27 +0200

    enh: preselect show_grid files instead of having remove_selected button

--- dashboard/hpc_pipeine_app/hsm_grid.py
+++ dashboard/hpc_pipeine_app/hsm_grid.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import pickle
 
-import dash
 import dash_ag_grid as dag
 from dash import callback_context as cc
 import dash_bootstrap_components as dbc
@@ -27,7 +26,7 @@ def create_hsm_grid():
     """Creates the HSMFS file explorer grid"""
     return html.Div(
         [
-            dcc.Store(id="store_dcor_paths", data=[]),
+            dcc.Store(id="store_dcor_files", data=[]),
             line_breaks(times=1),
             input_with_dropdown(
                 comp_id="input_group",
@@ -95,7 +94,7 @@ def create_hsm_grid():
 
 
 def create_show_grid(comp_id):
-    """Show the user selected data paths in a show grid"""
+    """Create show grid to display user selected files for final refining"""
     show_btn = dbc.Button(
         [
             "Selected files:",
@@ -105,8 +104,6 @@ def create_show_grid(comp_id):
         color="info",
         style={"margin-right": "10px"}
     )
-    remove_btn = dbc.Button("Remove Selected", id="remove_entries",
-                            color="info")
 
     show_grid = dag.AgGrid(
         id=comp_id,
@@ -144,7 +141,6 @@ def create_show_grid(comp_id):
         [
             dbc.Row([
                 dbc.Col(show_btn, width="auto"),
-                dbc.Col(remove_btn, width="auto"),
             ],
                 justify="center"
             ),
@@ -163,8 +159,7 @@ def create_show_grid(comp_id):
     Input("pipeline_accord", "active_item"),
 )
 def load_hms_grid_data(pipeline_active_accord):
-    """Show HSMFS grid data only when the user clicks on `Data to process`
-    accord"""
+    """Show HSMFS grid only when user clicks on `Data to Process` accord"""
     if pipeline_active_accord == "hsm_accord":
         return load_data_chunk(1)
     else:
@@ -173,77 +168,51 @@ def load_hms_grid_data(pipeline_active_accord):
 
 @callback(
     Output("show_grid", "rowData"),
-    Output("num_files", "children"),
+    Output("show_grid", "selectedRows"),
     Input("hsm_grid", "selectedRows"),
-    Input("store_dcor_paths", "data"),
+    Input("store_dcor_files", "data"),
     prevent_initial_call=True
 )
-def display_selected_paths(hsm_selection, stored_input):
-    """Collect the user selected data paths and send them to `show_grid`"""
-    original_paths = [] + stored_input
+def update_show_grid_data(hsm_selection, selected_files):
+    """Collect the user-selected data files and send them to `show_grid`"""
     if hsm_selection:
-        # Get the list of user selected hsmfs paths (each path is a list
-        # of strings) and join them with "/"
-        selected_paths = ["/".join(s["filepath"]) for s in hsm_selection]
-        original_paths = original_paths + selected_paths
-    if original_paths:
-        rowdata = [{"filepath": i} for i in original_paths]
-        return rowdata, len(original_paths)
-    else:
-        rowdata = [{"filepath": i} for i in original_paths]
-        return rowdata, len(original_paths)
+        hsm_files = ["/".join(s["filepath"]) for s in hsm_selection]
+        selected_files = selected_files + hsm_files
+    # Convert list of strings into ag grid rowdata
+    rowdata = [{"filepath": i} for i in selected_files]
+    return rowdata, rowdata
 
 
 @callback(
-    Output("store_dcor_paths", "data"),
+    Output("num_files", "children"),
+    Input("show_grid", "selectedRows"),
+    prevent_initial_call=True
+)
+def display_selected_files_number(show_grid_rows):
+    """Display the number of selected files"""
+    return len(show_grid_rows)
+
+
+@callback(
+    Output("store_dcor_files", "data"),
     Output("input_group_drop", "value"),
     Output("input_group_text", "value"),
     Input("input_group_button", "n_clicks"),
     Input("input_group_drop", "value"),
     Input("input_group_text", "value"),
-    State("show_grid", "selectedRows"),
-    Input("remove_entries", "n_clicks"),
-    State("store_dcor_paths", "data"),
+    State("store_dcor_files", "data"),
     prevent_initial_call=True
 )
-def store_input_group_paths(click_button, drop_input, text_input,
-                            show_selection, remove_click, cached_paths):
-    """Collects the user selected data paths and cache them"""
+def store_input_group_files(_, drop_input, text_input, cached_files):
+    """Collects the user selected dcor files and cache them"""
     button_triggered = cc.triggered[0]["prop_id"].split(".")[0]
 
     if button_triggered == "input_group_button":
         if text_input and drop_input:
             input_path = f"{drop_input}: {text_input}"
-            cached_paths.append(input_path)
-            return cached_paths, None, None
-    elif button_triggered == "remove_entries" and show_selection:
-        cached_paths = [path for path in cached_paths if
-                        path not in (sentry["filepath"] for sentry in
-                                     show_selection)]
-        return cached_paths, drop_input, text_input
+            cached_files.append(input_path)
+            return cached_files, None, None
     raise PreventUpdate
-
-
-@callback(
-    Output("hsm_grid", "selectedRows"),
-    Input("remove_entries", "n_clicks"),
-    State("show_grid", "selectedRows"),
-    State("hsm_grid", "selectedRows"),
-    prevent_initial_call=True
-)
-def update_hsm_grid_rowdata(click, show_selection, hsm_selection):
-    """When the user deselects the checkboxes in hsm grid or removes rows
-    from show grid, hsm checkboxes will be adjusted accordingly, and making
-    sure only  the user-selected paths should be present"""
-    if not click or not show_selection or not hsm_selection:
-        return dash.no_update
-    # Filter out matching entries from hms_selection. If a matching
-    # filepath is found, the corresponding entry is removed from the
-    # "hsm_grid" selection.
-    selected_paths = set(dic["filepath"] for dic in show_selection)
-    updated_hsm_selection = [hs for hs in hsm_selection if
-                             "/".join(hs["filepath"]) not in selected_paths]
-    return updated_hsm_selection
 
 
 @callback(
@@ -256,16 +225,3 @@ def update_filter(filter_value, gridOptions):
     """Filter grid rows based on filter value"""
     gridOptions["quickFilterText"] = filter_value
     return gridOptions
-
-
-@callback(
-    Output("remove_entries", "disabled"),
-    Input("show_grid", "selectedRows"),
-)
-def toggle_remove_entries_button(show_selection):
-    """Activates `remove entries` button only when user selects rows in
-    show grid rows"""
-    if show_selection:
-        return False
-    else:
-        return True
