f0358525cd2c72486a3183c58df6accf4a13154a
Raghava Alajangi <raghava.alajangi@mpl.mpg.de>
2023-10-20 12:15:03 +0000

    Revert "Merge branch 'add_user_dropdown' into 'main'"
    This reverts merge request !7

--- cache_handler.py
+++ cache_handler.py
@@ -4,32 +4,55 @@ import pickle
 import os
 import time
 
-HSM_PATH = Path(__file__).parents[1] / "HSMFS" / "Data"
+HSM_PATH = Path(__file__).parents[1] / "HSMFS"
 RESOURCE_PATH = Path(__file__).parents[0] / "resources"
 
 
 class HSMDataProcessor:
-    def __init__(self, drive_path, resource_path):
+    def __init__(self, drive_path, chunk_size, resource_path):
         self.drive_path = drive_path
+        self.chunk_size = chunk_size
         self.resource_path = resource_path
+        self.chunk_dir_path = resource_path / "hsm_chunk_dir"
+        self.chunk_idx_file_path = resource_path / "hsm_chunk_index.pkl"
+        if not self.chunk_dir_path.exists():
+            self.chunk_dir_path.mkdir(parents=True, exist_ok=True)
+        self.chunk_data = []
+        self.chunk_index = {}
 
-    def clear_resource_dir(self):
-        """Erase previously created HSMFS drive pickle from resource dir."""
-        for item in self.resource_path.iterdir():
+    def clear_chunk_directory(self):
+        """Erase chunk data and index files from resource dir."""
+        if self.chunk_idx_file_path.exists():
+            self.chunk_idx_file_path.unlink()
+        for item in self.chunk_dir_path.iterdir():
             if item.is_file():
                 item.unlink()
+            elif item.is_dir():
+                item.rmdir()
 
-    def save_hsm_data(self, hsm_data):
-        """Save extracted rtdc paths from HSMFS as a pickle file in
-        resource dir."""
-        with open(self.resource_path / "hsm_drive.pkl", "wb") as f:
-            pickle.dump(hsm_data, f)
+    def save_chunk_data(self, chunk_data, chunk_id):
+        """Save the chunk data as a pickle file in hsm_chunk_dir."""
+        chunk_path = self.chunk_dir_path / f"chunk_num_{chunk_id}.pkl"
+        with open(chunk_path, "wb") as f:
+            pickle.dump(chunk_data, f)
+
+    def update_and_save_chunk_index(self, chunk_data, chunk_id):
+        """Update the chunk index and save it."""
+        for entry in chunk_data:
+            for keyword in entry["filepath"]:
+                if keyword not in self.chunk_index:
+                    self.chunk_index[keyword] = set()
+                self.chunk_index[keyword].add(chunk_id)
+
+        with open(str(self.chunk_idx_file_path), "wb") as f:
+            pickle.dump(self.chunk_index, f)
 
     def process_drive(self):
         t1 = time.time()
 
-        self.clear_resource_dir()
-        hsm_data = []
+        self.clear_chunk_directory()
+        chunk_data = []
+        chunk_counter = 0
 
         for dirpath, dirnames, filenames in os.walk(self.drive_path):
             filenames = [f for f in filenames if f.endswith(".rtdc")]
@@ -40,24 +63,42 @@ class HSMDataProcessor:
                 # Get the modified date of the RTDC file
                 modified_time = os.path.getmtime(fpath)
                 modified_time = dt.fromtimestamp(
-                    modified_time).strftime("%d-%b-%Y %I.%M %p")
-                # Get the path without HSMFS drive name
-                fpath_wo_hsm = fpath.split("HSMFS/")[1]
-                # Split path string into list and add HSMFS: drive identifier
-                file_path_list = ["HSMFS:"] + list(fpath_wo_hsm.split("/"))
+                    modified_time).strftime(
+                    "%d-%b-%Y %I.%M %p")
+                # Get the relative path without HSMFS drive name
+                fpath_wo_hsmfs = fpath.split("HSMFS/")[1]
+                # Split path string into list and add HSMFS identifier
+                file_path_list = ["HSMFS:"] + list(fpath_wo_hsmfs.split("/"))
                 entry = {
                     "filepath": file_path_list,
                     "dateModified": modified_time,
                 }
-                hsm_data.append(entry)
+                chunk_data.append(entry)
 
-        # Pickle the hsm data
-        self.save_hsm_data(hsm_data)
+                # Pickle the chunk data when it reaches the chunk size
+                if len(chunk_data) >= self.chunk_size:
+                    chunk_counter += 1
+                    self.save_chunk_data(chunk_data, chunk_counter)
+                    # Update and save the final chunk index
+                    self.update_and_save_chunk_index(chunk_data, chunk_counter)
+                    chunk_data = []
+
+        # Pickle any remaining data if it's not a full chunk
+        if chunk_data:
+            chunk_counter += 1
+            self.save_chunk_data(chunk_data, chunk_counter)
+            # Update and save the final chunk index
+            self.update_and_save_chunk_index(chunk_data, chunk_counter)
+
+        # Save the chunk index file
+        with open(str(self.chunk_idx_file_path), "wb") as f:
+            pickle.dump(self.chunk_index, f)
 
         disc_time = str(timedelta(seconds=time.time() - t1)).split(".")[0]
         print(f"Disc scanning time: {disc_time}")
 
 
 if __name__ == "__main__":
-    processor = HSMDataProcessor(HSM_PATH, RESOURCE_PATH)
+    CHUNK_SIZE = 2000000
+    processor = HSMDataProcessor(HSM_PATH, CHUNK_SIZE, RESOURCE_PATH)
     processor.process_drive()
