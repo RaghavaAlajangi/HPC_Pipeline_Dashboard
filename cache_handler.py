9adad46ba1dec10f3379ed2911ce696ca4b37878
ralajan <raghava.alajangi@mpl.mpg.de>
2023-12-22 12:47:29 +0100

    enh: exclude less than 1Mb files from HSMFS grid

--- cache_handler.py
+++ cache_handler.py
@@ -4,7 +4,7 @@ import pickle
 import os
 import time
 
-# Restrict the dashboard to scan only `Data` directory from HSMFS
+# Restrict the dashboard to scan only `Data` directory from mounted HSMFS
 HSM_PATH = Path(__file__).parents[1] / "HSMFS" / "Data"
 RESOURCE_PATH = Path(__file__).parents[0] / "resources"
 
@@ -22,6 +22,17 @@ class HSMDataProcessor:
             if item.is_file():
                 item.unlink()
 
+    @staticmethod
+    def get_file_size(file_path):
+        """Compute size the file"""
+        file_size_bytes = os.path.getsize(file_path)
+        if file_size_bytes < 1024 ** 2:  # Less than 1 MB
+            return None
+        elif file_size_bytes < 1024 ** 3:  # Less than 1 GB
+            return f"{file_size_bytes / (1024 ** 2):.1f} MB"
+        else:
+            return f"{file_size_bytes / (1024 ** 3):.1f} GB"
+
     def save_hsm_data(self, hsm_data):
         """Save extracted rtdc paths from HSMFS as a pickle file in
         resource dir."""
@@ -38,21 +49,24 @@ class HSMDataProcessor:
             filenames = [f for f in filenames if f.endswith(".rtdc")]
             for fname in filenames:
                 fpath = os.path.join(dirpath, fname)
-                # Standardize the RTDC file path
-                fpath = fpath.replace("\\", "/").replace("//", "/")
-                # Get the modified date of the RTDC file
-                modified_time = os.path.getmtime(fpath)
-                modified_time = dt.fromtimestamp(
-                    modified_time).strftime("%d-%b-%Y %I.%M %p")
-                # Get the path without HSMFS drive name
-                fpath_wo_hsm = fpath.split("HSMFS/")[1]
-                # Split path string into list and add HSMFS: drive identifier
-                file_path_list = ["HSMFS:"] + list(fpath_wo_hsm.split("/"))
-                entry = {
-                    "filepath": file_path_list,
-                    "dateModified": modified_time,
-                }
-                hsm_data.append(entry)
+                file_size = self.get_file_size(fpath)
+                if file_size:
+                    # Standardize the RTDC file path
+                    fpath = fpath.replace("\\", "/").replace("//", "/")
+                    # Get the modified date of the RTDC file
+                    modified_time = os.path.getmtime(fpath)
+                    modified_time = dt.fromtimestamp(
+                        modified_time).strftime("%d-%b-%Y %I.%M %p")
+                    # Get the path without HSMFS drive name
+                    fpath_wo_hsm = fpath.split("HSMFS/")[1]
+                    # Split path into string list, add HSMFS: drive identifier
+                    file_path_list = ["HSMFS:"] + list(fpath_wo_hsm.split("/"))
+                    entry = {
+                        "filepath": file_path_list,
+                        "dateModified": modified_time,
+                        "size": file_size
+                    }
+                    hsm_data.append(entry)
 
         # Pickle the hsm data
         self.save_hsm_data(hsm_data)
